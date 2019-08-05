4399da9110fc316c7ae3d08441a1558fcb3850b4
n1l <dhvega@yandex.ru>
2019-08-05 13:38:13 +0300

    Add new tasks

---
+++ white/tasks/src/week03/task03/sorted_strings.h
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class SortedStrings
+{
+public:
+  void AddString(const string &s)
+  {
+    _values.push_back(s);
+
+    sort(begin(_values), end(_values));
+  }
+  vector<string> GetSortedStrings()
+  {
+    return _values;
+  }
+
+private:
+  vector<string> _values;
+};

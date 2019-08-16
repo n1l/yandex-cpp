642fd5559f9214764b48f7823a50dbafa7d5beb5
n1l <dhvega@yandex.ru>
2019-08-16 20:11:07 +0300

    Add new task from kata's site

---
+++ white/tasks/src/week04/132_pattern/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool find132pattern(vector<int> &nums)
+{
+  for (int i = 0; i < nums.size() - 2; i++)
+  {
+    for (int j = i + 1; j < nums.size() - 1; j++)
+    {
+      for (int k = j + 1; k < nums.size(); k++)
+      {
+        if (nums[k] > nums[i] && nums[j] > nums[k])
+        {
+          cout << nums[i] << ' ' << nums[k] << ' ' << nums[j];
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+int main()
+{
+  vector<int> n = {1, 2, 3, 4};
+  cout << (find132pattern(n) ? "True" : "False") << endl;
+
+  n = {3, 1, 4, 2};
+  cout << (find132pattern(n) ? "True" : "False") << endl;
+
+  n = {-1, 3, 2, 0};
+  cout << (find132pattern(n) ? "True" : "False") << endl;
+
+  n = {3, 5, 0, 3, 4};
+  cout << (find132pattern(n) ? "True" : "False") << endl;
+
+  return 0;
+}
